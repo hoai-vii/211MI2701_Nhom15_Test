3c938366231fb3b0ca4f9947964c78938a9294d4
maithithuyhau <haumtt19411@st.uel.edu.vn>
2021-12-12 10:36:21 +0700

    model Product

--- app/src/main/java/com/example/model/Product.java
+++ app/src/main/java/com/example/model/Product.java
@@ -1,4 +1,57 @@
 package com.example.model;
 
 public class Product {
+    private int MaSP;
+    private String productName;
+    private String HangSX;
+    private Float Price;
+    private byte[] imageProduct;
+
+    public void setMaSP(int maSP) {
+        MaSP = maSP;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setHangSX(String hangSX) {
+        HangSX = hangSX;
+    }
+
+    public void setPrice(Float price) {
+        Price = price;
+    }
+
+    public void setImageProduct(byte[] imageProduct) {
+        this.imageProduct = imageProduct;
+    }
+
+    public int getMaSP() {
+        return MaSP;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getHangSX() {
+        return HangSX;
+    }
+
+    public Float getPrice() {
+        return Price;
+    }
+
+    public byte[] getImageProduct() {
+        return imageProduct;
+    }
+
+    public Product(int maSP, String productName, String hangSX, Float price, byte[] imageProduct) {
+        MaSP = maSP;
+        this.productName = productName;
+        HangSX = hangSX;
+        Price = price;
+        this.imageProduct = imageProduct;
+    }
 }
